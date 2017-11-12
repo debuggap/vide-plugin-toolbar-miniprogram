02d1e7fef94e738fd9fd94132015cb311abf04a6
vic.wang163 <vic.wang163@gmail.com>
2017-11-12 20:35:03 +0800

    finish api transform

---
+++ do.js
@@ -0,0 +1,4 @@
+transformMiniprogram = require('./lib/index')
+
+let form = {type: 'w2a', src:'/Volumes/ext/weixin/aaa/'};
+transformMiniprogram(form)
--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,57 @@
 <template>
-  <div style="z-index:100">
-    vide-plugin-toolbar-miniprogram
+  <div class="miniprogram" style="z-index:999">
+    <el-form ref="form" :model="form" label-position="left" label-width="100px">
+      <el-form-item label="小程序转换:">
+        <el-radio v-model="form.type" label="w2a">微信转支付宝</el-radio>
+      </el-form-item>
+      <el-form-item label="项目路径:">
+        <el-input v-model="form.src" >
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="transform">转换</el-button>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 
 <script>
+import {Form, FormItem, Input, Radio} from 'element-ui'
 
 export default {
   data () {
     return {
-      name: 'VIDE'
+      form: {
+        type: 'w2a',
+        src: '/Volumes/ext/weixin/aaa/'
+      }
+    }
+  },
+  components: {
+    'el-form': Form,
+    'el-form-item': FormItem,
+    'el-input': Input,
+    'el-radio': Radio
+  },
+  methods: {
+    transform () {
+      alert(1)
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-
+.miniprogram {
+  width:600px;
+  height:500px;
+  position: absolute;
+  left: 50%;
+  margin-left: -300px;
+  top: 50%;
+  margin-top: -250px;
+  background-color: #fff;
+  border-radius: 10px;
+  padding: 10px;
+  box-sizing: border-box;
+}
 </style>
